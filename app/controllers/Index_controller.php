e994f01ece282a141a1156b11841fee8865bc9ca
Aaron Munguia <aamm89@gmail.com>
2014-07-11 19:51:49 -0500

    add login

--- app/controllers/Index_controller.php
+++ app/controllers/Index_controller.php
@@ -6,7 +6,12 @@ class Index_controller extends AppController{
      * Login users
      * @return void 
      */
-    public function index(){
+    public function index($page = null){
+
+        $user = new User();
+        $this->view->user = $user;
+        
+        $this->view->setLayout('index');
         $this->render();
     }
 }
--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,3 +1,5 @@
 <h2>Login</h2>
 
+<?php $this->renderElement('messages'); ?>
+
 <?php $this->renderElement('login.form'); ?>
