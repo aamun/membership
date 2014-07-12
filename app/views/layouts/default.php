18681e27f691a555e77d303080a287e599c0c21e
Aaron Munguia <aamm89@gmail.com>
2014-07-11 19:53:09 -0500

    improve default layout

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
-<html lang="en" xml:lang="en">
-	<head>
-    	<title><?php echo $title_for_layout; ?></title>
-		<meta name="generator" content="flavorPHP" />
-        <?php echo $this->html->charsetTag("UTF-8"); ?>		
-	</head>
+<html>
+    <head>
+        <title><?php echo $title_for_layout; ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+        <meta name="author" content="Aarón Munguía">
+        <meta name="generator" content="flavorPHP" />
+        <?php echo $this->html->charsetTag("UTF-8"); ?>
+        <link rel="stylesheet" href="<?php echo Path; ?>/bower_components/bootstrap/dist/css/bootstrap.min.css">
+        <?php echo $this->html->includeCSS('skin'); ?>
+
+        <script src="<?php echo Path; ?>/bower_components/jquery/dist/jquery.min.js"></script>
+        <script src="<?php echo Path; ?>/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    </head>
     <body>
-        <div>
-            <?php echo $content_for_layout ?>
-        </div>
+        <?php echo $content_for_layout ?>
     </body>
 </html>
