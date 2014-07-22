ec9774b41779b28678d428457d0fae9545be6dba
Aaron Munguia <aamm89@gmail.com>
2014-07-22 12:09:18 -0500

    implement count items method in members

--- app/models/Member.php
+++ app/models/Member.php
@@ -3,6 +3,10 @@
 * Member
 */
 class Member extends AppModel {
-    
+    public function count(){
+        $this->db->query("SELECT COUNT(idMember) AS total FROM members");
+        $row = $this->db->fetchRow();
+        return $row['total'];
+    }
 }
 ?>
