46bfed486ec63e24ede36eded437385a5b9b3683
Aaron Munguia <aamm89@gmail.com>
2014-07-22 12:26:12 -0500

    some description about makerspace

--- app/views/index/index.php
+++ app/views/index/index.php
@@ -1,3 +1,29 @@
-<h2>Login</h2>
+<h1>Makers Space</h1>
 
-<?php $this->renderElement('login.form'); ?>
+<p>
+    Makers Space es un espacio de colaboración Maker. <br>
+</p>
+
+<h3>Espacio</h3>
+<p>
+    Espacios ...
+</p>
+
+
+<h3>Herramientas</h3>
+<p>
+    Contamos con varias herramientas como CNC, Impresora 3D, Taladros, Cautines, Tarjetas de prototipado.
+</p>
+
+<h3>Membresias</h3>
+<p>
+    Actualmente contamos con 2 tipos de membresias.
+</p>
+<p>
+    Normal. <br>
+    Que tiene acceso a ...
+</p>
+<p>
+    Estudiante. <br>
+    Si eres estudiante tendras los mismos beneficios que la normal pero con un precio más economico
+</p>
